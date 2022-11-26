f4c2cd8505e7b3ef016cc8738db2a1e60ef0ef5f
Srushti <srushti1010shah@gmail.com>
2022-11-27 02:42:58 +0530

    Premiere carousel styling done

--- src/components/Premiere/Premiere.component.jsx
+++ src/components/Premiere/Premiere.component.jsx
@@ -115,9 +115,13 @@ const Premiere = () => {
   };
   return (
     <>
+      <div className="flex flex-col items-start">
+        <h3 className="text-white text-xl font-bold">Premieres</h3>
+        <p className="text-white text-sm">Brand new releases every Friday</p>
+      </div>
       <Slider {...settings}>
         {PremiereImages.map((image) => (
-          <Poster {...image} />
+          <Poster {...image} isDark />
         ))}
       </Slider>
     </>
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -6,12 +6,24 @@ import Premiere from "../components/Premiere/Premiere.component";
 const HomePage = () => {
   return (
     <>
-      <div className="container mx-auto px-4">
-        <h4 className="text-2xl font-bold text-gray-800 my-3">
-          The Best of Entertainment
-        </h4>
-        <EntertainmentCardSlider />
-        <Premiere />
+      <div className="flex flex-col gap-10">
+        <div className="container mx-auto px-4">
+          <h4 className="text-2xl font-bold text-gray-800 my-3">
+            The Best of Entertainment
+          </h4>
+          <EntertainmentCardSlider />
+        </div>
+        <div className="bg-bmsBlue-800 py-12">
+          <div className="container mx-auto px-4 flex flex-col gap-3">
+            <div className="hidden md:flex">
+              <img
+                src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+                alt="Premiere and Rupay"
+              />
+            </div>
+            <Premiere />
+          </div>
+        </div>
       </div>
     </>
   );
