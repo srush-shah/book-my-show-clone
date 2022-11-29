7330a887e53f8771995f47c3e4e393eeafb9239b
Srushti <srushti1010shah@gmail.com>
2022-11-30 00:36:37 +0530

    Added TMDB api, its configuration and trending posters on Hero Carousal

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 //HOC
 import DefaultHOC from "./HOC/Default.HOC";
 import MovieHOC from "./HOC/Movie.HOC";
@@ -10,6 +12,11 @@ import Plays from "./pages/Plays.page";
 //CSS Files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+//Axios default settings
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 function App() {
   return (
--- src/components/HeroCarousel/HeroCarousel.component.jsx
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeroSlider from "react-slick";
+import axios from "axios";
 
 //Arrows
 import { NextArrow, PrevArrow } from "./Arrows.component";
 
 const HeroCarousel = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const requestNowPlayingMovies = async () => {
+      const getImages = await axios.get("/movie/now_playing");
+      console.log(getImages);
+      setImages(getImages.data.results);
+    };
+    requestNowPlayingMovies();
+  }, []);
+
   const settingsLG = {
     arrows: true,
     autoplay: true,
@@ -26,15 +38,6 @@ const HeroCarousel = () => {
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
   };
-
-  const images = [
-    "https://images.unsplash.com/photo-1661961110671-77b71b929d52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1669042490014-6a1b76025e0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1669111959281-7f4cdd990620?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-    "https://plus.unsplash.com/premium_photo-1664373233201-8b9b80b59d1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1669051759318-e3f68b839d1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1669039643095-3f3de0351155?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-  ];
   return (
     <>
       <div className="lg:hidden">
@@ -42,7 +45,7 @@ const HeroCarousel = () => {
           {images.map((image) => (
             <div className="w-full h-56 md:h-80 py-3">
               <img
-                src={image}
+                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
                 alt="testing"
                 className="w-full h-full rounded-md"
               />
@@ -55,7 +58,7 @@ const HeroCarousel = () => {
           {images.map((image) => (
             <div className="w-full h-96 px-2 py-3">
               <img
-                src={image}
+                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
                 alt="testing"
                 className="w-full h-full rounded-md"
               />
